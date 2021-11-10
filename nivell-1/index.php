8eca4fc41c78138927686a74f04a022113c7f782
Albert Garcia Llorca <albertgarcia20@gmail.com>
2021-11-10 09:01:48 +0000

    Nivell 1 / Exercici 2 finalitzat

--- nivell-1/index.php
+++ nivell-1/index.php
@@ -23,3 +23,43 @@ $empleat->initialize("Albert", 5500);
 $empleat->print();
 $empleat->initialize("Ramon", 6500);
 $empleat->print();
+
+echo "<br>";
+echo "<h1>Exercici 2</h1>";
+
+class Shape {
+    protected $ample;
+    protected $alt;
+
+    public function __construct($ample, $alt){
+        $this->ample = $ample;
+        $this->alt = $alt;
+    }
+}
+
+class Triangle extends Shape {
+
+    public function __construct($ample, $alt){
+        Shape::__construct($ample, $alt);
+    }
+
+    public function area(){
+        return ($this->ample / 2) * $this->alt;
+    }
+}
+
+class Rectangle extends Shape {
+
+    public function __construct($ample, $alt){
+        Shape::__construct($ample, $alt);
+    }
+
+    public function area(){
+        return $this->ample * $this->alt;
+    }
+}
+
+$forma1 = new Triangle(5,3);
+$forma2 = new Rectangle(5,5);
+echo "L'àrea del triangle és ". $forma1->area() . "<br>";//Esperat 7.5
+echo "L'àrea del rectangle és ". $forma2->area() . "<br>";//Esperat 25
